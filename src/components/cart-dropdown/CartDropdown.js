e260cdc3c8dba6b911ca989786e83f07f4bb6aa1
Nnamdi ujah <nnamdi@softcom.ng>
2020-05-25 02:25:30 +0100

    added cartItems component to cart dropdown

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,12 +1,16 @@
 import React from 'react';
-
+import {useSelector} from "react-redux";
+import CartItem from "../cart-item/Cart-Item";
 import {Button} from "../form/Form"
 import "../../styles/cart-dropdown.scss";
 
 const Cart = () => {
+    const {cartItems} = useSelector(state => state.cart)
     return ( 
         <div className="cart-dropdown">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {cartItems.map(item => <CartItem key={item.id} item={item} />)}
+            </div>
             <Button title="Go To Checkout"/>
         </div>
      );
